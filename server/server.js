8dcccd0715b8d3854849ed9d1cdd46f26d98ca45
Kirill Chernik <kirillchernik8@gmail.com>
2019-08-17 17:57:07 -0400

    fix deployment error

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ app.use(parser.json())
 app.use(express.static(path.join(__dirname, '../public')))
 app.use(compression())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/api/allUsers', (req, res) => {
   getUserInfo((result) => {
     res.send(result)
